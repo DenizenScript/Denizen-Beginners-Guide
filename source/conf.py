cc8ce01ae95f91dc73ac2cefa8a1fab35ae3a872
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-07-01 04:28:56 -0700

    add r/g/b script sample classifier

--- source/conf.py
+++ source/conf.py
@@ -27,57 +27,66 @@ def setup(app):
 
 templates_path = ['_templates']
 
-# dScript highlighting placeholder
-class dScriptLexer(RegexLexer):
-    name = 'dscript'
-    tokens = {
-        'spaces_patch': [
-            (r'\s', token.Text ) #                                                    spaces
-        ],
-        'inside_brackets': [
-            (r'\[', token.Name.Variable, '#push'), #                                  [
-            (r'\]', token.Name.Variable, '#pop'), #                                   ]
-            include('tag_finder'),
-            (r'$', token.Text, '#pop'),
-            (r'.', token.Name.Variable) #                                             anything else
-        ],
-        'inside_tag': [
-            (r'\[(?=([^\s]+)\])', token.Name.Variable, 'inside_brackets' ), #         [brackets]
-            (r'\.', token.Operator ), #                                               .
-            (r'>', token.Name.Tag, '#pop'), #                                         >
-            (r'$', token.Text, '#pop'),
-            (r'.', token.Name.Tag) #                                                  anything else
-        ],
-        'tag_finder': [
-            (r'<(?=([^\s]+)>)', token.Name.Tag, 'inside_tag' ), #                     <tag>
-            (r'%.*%', token.Generic.Error ) #                                         %old_def%
-        ],
-        'double_quoted': [
-            include('tag_finder'),
-            (r'"', token.Literal.String, '#pop'), #                                   ]
-            (r'.', token.Literal.String ) #                                           anything else
-        ],
-        'single_quoted': [
-            include('tag_finder'),
-            (r'\'', token.Literal.String.Backtick, '#pop'), #                         ]
-            (r'.', token.Literal.String.Backtick ) #                                  anything else
-        ],
-        'code_line': [
-            (r'"(?=([^"]+)")', token.Literal.String, 'double_quoted' ), #             "text"
-            (r'\'(?=([^\']+)\')', token.Literal.String.Backtick, 'single_quoted' ), # 'text'
-            (r'$', token.Text, '#pop'),
-            include('tag_finder'),
-            (r'.', token.Text ) #                                                     anything else
-        ],
-        'root': [
-            (r'^\s*#\s*[\|+=].*$', token.Comment.Hashbang ), #                        # +--- header comment
-            (r'^\s*#\s*-.*$', token.Comment.Single ), #                               # - code comment
-            (r'^\s*#.*$', token.Comment ), #                                          # regular comment
-            (r'^[^-#:]*:', token.Name.Class ), #                                       yaml key:
-            (r'^\s*-\s[^\s]+\s', token.Keyword, 'code_line' ), #                      - somecommand
-            include('spaces_patch'),
-            (r'.', token.Text ) #                                                     anything else
-        ]
-    }
+reusable_tokens = {
+    'spaces_patch': [
+        (r'\s', token.Text ) #                                                    spaces
+    ],
+    'inside_brackets': [
+        (r'\[', token.Name.Variable, '#push'), #                                  [
+        (r'\]', token.Name.Variable, '#pop'), #                                   ]
+        include('tag_finder'),
+        (r'$', token.Text, '#pop'),
+        (r'.', token.Name.Variable) #                                             anything else
+    ],
+    'inside_tag': [
+        (r'\[(?=([^\s]+)\])', token.Name.Variable, 'inside_brackets' ), #         [brackets]
+        (r'\.', token.Operator ), #                                               .
+        (r'>', token.Name.Tag, '#pop'), #                                         >
+        (r'$', token.Text, '#pop'),
+        (r'.', token.Name.Tag) #                                                  anything else
+    ],
+    'tag_finder': [
+        (r'<(?=([^\s]+)>)', token.Name.Tag, 'inside_tag' ), #                     <tag>
+        (r'%.*%', token.Generic.Error ) #                                         %old_def%
+    ],
+    'double_quoted': [
+        include('tag_finder'),
+        (r'"', token.Literal.String, '#pop'), #                                   ]
+        (r'.', token.Literal.String ) #                                           anything else
+    ],
+    'single_quoted': [
+        include('tag_finder'),
+        (r'\'', token.Literal.String.Backtick, '#pop'), #                         ]
+        (r'.', token.Literal.String.Backtick ) #                                  anything else
+    ],
+    'code_line': [
+        (r'"(?=([^"]+)")', token.Literal.String, 'double_quoted' ), #             "text"
+        (r'\'(?=([^\']+)\')', token.Literal.String.Backtick, 'single_quoted' ), # 'text'
+        (r'$', token.Text, '#pop'),
+        include('tag_finder'),
+        (r'.', token.Text ) #                                                     anything else
+    ],
+    'root': [
+        (r'^\s*#\s*[\|+=].*$', token.Comment.Hashbang ), #                        # +--- header comment
+        (r'^\s*#\s*-.*$', token.Comment.Single ), #                               # - code comment
+        (r'^\s*#.*$', token.Comment ), #                                          # regular comment
+        (r'^[^-#:]*:', token.Name.Class ), #                                       yaml key:
+        (r'^\s*-\s[^\s]+\s', token.Keyword, 'code_line' ), #                      - somecommand
+        include('spaces_patch'),
+        (r'.', token.Text ) #                                                     anything else
+    ]
+}
 
-lexers['dscript'] = dScriptLexer(startinline=True)
+class dScriptLexerRed(RegexLexer):
+    name = 'dscript_red'
+    tokens = reusable_tokens
+class dScriptLexerGreen(RegexLexer):
+    name = 'dscript_green'
+    tokens = reusable_tokens
+class dScriptLexerBlue(RegexLexer):
+    name = 'dscript_blue'
+    tokens = reusable_tokens
+
+lexers['dscript_red'] = dScriptLexerRed(startinline=True)
+lexers['dscript_green'] = dScriptLexerGreen(startinline=True)
+lexers['dscript_blue'] = dScriptLexerBlue(startinline=True)
